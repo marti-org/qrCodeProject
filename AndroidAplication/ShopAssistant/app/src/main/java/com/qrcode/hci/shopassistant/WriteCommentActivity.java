e38db3fffdba9c20dc09ea89b92e14a9dafd3ead
marti-org <martinorava@seznam.cz>
2015-04-14 23:14:59 +0300

    Design changes on product page and wirte comment page

--- AndroidAplication/ShopAssistant/app/src/main/java/com/qrcode/hci/shopassistant/WriteCommentActivity.java
+++ AndroidAplication/ShopAssistant/app/src/main/java/com/qrcode/hci/shopassistant/WriteCommentActivity.java
@@ -1,9 +1,11 @@
 package com.qrcode.hci.shopassistant;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class WriteCommentActivity extends ActionBarActivity {
@@ -38,5 +40,10 @@ public class WriteCommentActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void btnSaveMessageClick(View view){
+        Intent intent = new Intent(this, ProductPage.class);
+        startActivity(intent);
     }
 }
