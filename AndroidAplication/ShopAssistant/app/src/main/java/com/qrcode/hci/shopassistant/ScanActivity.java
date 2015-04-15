23e150ee163dba98c74146df680dc3fec4bc60f5
marti-org <martinorava@seznam.cz>
2015-04-15 20:05:19 +0300

    Scaning functionality

--- AndroidAplication/ShopAssistant/app/src/main/java/com/qrcode/hci/shopassistant/ScanActivity.java
+++ AndroidAplication/ShopAssistant/app/src/main/java/com/qrcode/hci/shopassistant/ScanActivity.java
@@ -1,19 +1,31 @@
 package com.qrcode.hci.shopassistant;
 
 import android.content.Intent;
+import android.graphics.PointF;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
 
 
-public class ScanActivity extends ActionBarActivity {
+public class ScanActivity extends ActionBarActivity implements QRCodeReaderView.OnQRCodeReadListener {
+
+    private TextView myTextView;
+    private QRCodeReaderView mydecoderview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        mydecoderview = (QRCodeReaderView) findViewById(R.id.qrdecoderview);
+        mydecoderview.setOnQRCodeReadListener(this);
+
+        myTextView = (TextView) findViewById(R.id.exampleTextView);
     }
 
 
@@ -49,4 +61,45 @@ public class ScanActivity extends ActionBarActivity {
         Intent intent = new Intent(this, SearchActivity.class);
         startActivity(intent);
     }
+
+
+    //Scaning method
+    // Called when a QR is decoded
+    // "text" : the text encoded in QR
+    // "points" : points where QR control points are placed
+    @Override
+    public void onQRCodeRead(String text, PointF[] points) {
+
+        myTextView.setText(text);
+        Intent intent = new Intent(this, ProductPage.class);
+        startActivity(intent);
+
+    }
+
+
+    // Called when your device have no camera
+    @Override
+    public void cameraNotFound() {
+
+    }
+
+    // Called when there's no QR codes in the camera preview image
+    @Override
+    public void QRCodeNotFoundOnCamImage() {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mydecoderview.getCameraManager().startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mydecoderview.getCameraManager().stopPreview();
+    }
+
+
 }
