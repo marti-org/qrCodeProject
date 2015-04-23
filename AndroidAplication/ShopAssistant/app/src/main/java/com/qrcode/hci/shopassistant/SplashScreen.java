674243879ac977f1fc86f8159900a849f8d35ce6
ezgiyamac <ezgiyamac@yahoo.com.tr>
2015-04-23 16:51:31 +0300

    Basic Search Page

---
+++ AndroidAplication/ShopAssistant/app/src/main/java/com/qrcode/hci/shopassistant/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.qrcode.hci.shopassistant;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by eeristi on 23.4.2015.
+ */
+public class SplashScreen  extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        Thread splash_screen = new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    sleep(3000);
+
+                    startActivity(new Intent("com.qrcode.hci.shopassistant.ScanActivity"));
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                finally {
+                    finish();
+
+                }
+
+            }
+        };
+
+
+        splash_screen.start();
+
+    }
+}
