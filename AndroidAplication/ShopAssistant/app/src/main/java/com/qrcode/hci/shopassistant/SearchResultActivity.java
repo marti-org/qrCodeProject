f843f8846666b50d3c7e6493e58eef80f543d10f
ezgiyamac <ezgiyamac@yahoo.com.tr>
2015-04-23 05:22:37 +0300

    First View of Search Results Screen
    Search Results Screen Finished but there some problems still. I didn't
    fill rating bar for now and the listview is NOT clickable

--- AndroidAplication/ShopAssistant/app/src/main/java/com/qrcode/hci/shopassistant/SearchResultActivity.java
+++ AndroidAplication/ShopAssistant/app/src/main/java/com/qrcode/hci/shopassistant/SearchResultActivity.java
@@ -7,8 +7,60 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class SearchResultActivity extends ActionBarActivity {
+
+    // Array of strings storing country names
+    String[] products = new String[]
+            {
+                    "Watch",
+                    "Tablet",
+                    "Iphone 5S 16GB SMART PHONE",
+                    "Samsung I9190 GALAXY S4 MINI WHITE",
+                    "Iphone 6 16GB GOLD SMART PHONE",
+                    "Samsung I9190 GALAXY S6 WHITE 32GB",
+                    "LG723 G3 BEAT TITAN SMART PHONE ",
+                    "Samsung I9301 GALAXY S3 NEO WHITE",
+                    "Samsung I8200 GALAXY S3 MINI WHITE",
+                    "Samsung I9060 GALAXY GRAND NEO PLUS BLACK"
+            };
+
+
+    int[] flags = new int[]
+            {
+                    R.drawable.za,
+                    R.drawable.zb,
+                    R.drawable.zc,
+                    R.drawable.zd,
+                    R.drawable.zf,
+                    R.drawable.zg,
+                    R.drawable.zh,
+                    R.drawable.zi,
+                    R.drawable.zj,
+                    R.drawable.zk
+            };
+
+    // Array of strings to store currencies
+    String[] currency = new String[]
+            {
+                    "\n" + "\t1 Material: Silicone and Aluminium \n" + "\t2 Weight:2.68Oz/76g \n" + "\t3 Color:White \n" + "\t4 Dimensions:\n" + "\t\t(9.45x1.57x0.39)''/(8x4x1)cm (LxWxH) \n"  + "\t5 Package Includes:\n" + "\t\t1 x Smart Watch \n"  + "\t\t1 x USB Cable \n"  + "\t\t1 x User Manual \n" ,
+                    "\n" + "\t1 Material: Silicone and Aluminium \n" + "\t2 Weight:2.68Oz/76g \n" + "\t3 Color:White \n" + "\t4 Dimensions:\n" + "\t\t(9.45x1.57x0.39)''/(8x4x1)cm (LxWxH) \n"  + "\t5 Package Includes:\n" + "\t\t1 x Smart Watch \n"  + "\t\t1 x USB Cable \n"  + "\t\t1 x User Manual \n" ,
+                    "\n" + "\t1 Material: Silicone and Aluminium \n" + "\t2 Weight:2.68Oz/76g \n" + "\t3 Color:White \n" + "\t4 Dimensions:\n" + "\t\t(9.45x1.57x0.39)''/(8x4x1)cm (LxWxH) \n"  + "\t5 Package Includes:\n" + "\t\t1 x USB Cable \n"  + "\t\t1 x User Manual \n" ,
+                    "\n" + "\t1 Material: Silicone and Aluminium \n" + "\t2 Weight:2.68Oz/76g \n" + "\t3 Color:White \n" + "\t4 Dimensions:\n" + "\t\t(9.45x1.57x0.39)''/(8x4x1)cm (LxWxH) \n"  + "\t5 Package Includes:\n" + "\t\t1 x USB Cable \n"  + "\t\t1 x User Manual \n" ,
+                    "\n" + "\t1 Material: Silicone and Aluminium \n" + "\t2 Weight:2.68Oz/76g \n" + "\t3 Color:White \n" + "\t4 Dimensions:\n" + "\t\t(9.45x1.57x0.39)''/(8x4x1)cm (LxWxH) \n"  + "\t5 Package Includes:\n" + "\t\t1 x USB Cable \n"  + "\t\t1 x User Manual \n" ,
+                    "\n" + "\t1 Material: Silicone and Aluminium \n" + "\t2 Weight:2.68Oz/76g \n" + "\t3 Color:White \n" + "\t4 Dimensions:\n" + "\t\t(9.45x1.57x0.39)''/(8x4x1)cm (LxWxH) \n"  + "\t5 Package Includes:\n" + "\t\t1 x USB Cable \n"  + "\t\t1 x User Manual \n" ,
+                    "\n" + "\t1 Material: Silicone and Aluminium \n" + "\t2 Weight:2.68Oz/76g \n" + "\t3 Color:White \n" + "\t4 Dimensions:\n" + "\t\t(9.45x1.57x0.39)''/(8x4x1)cm (LxWxH) \n"  + "\t5 Package Includes:\n" + "\t\t1 x USB Cable \n"  + "\t\t1 x User Manual \n" ,
+                    "\n" + "\t1 Material: Silicone and Aluminium \n" + "\t2 Weight:2.68Oz/76g \n" + "\t3 Color:White \n" + "\t4 Dimensions:\n" + "\t\t(9.45x1.57x0.39)''/(8x4x1)cm (LxWxH) \n"  + "\t5 Package Includes:\n" + "\t\t1 x USB Cable \n"  + "\t\t1 x User Manual \n" ,
+                    "\n" + "\t1 Material: Silicone and Aluminium \n" + "\t2 Weight:2.68Oz/76g \n" + "\t3 Color:White \n" + "\t4 Dimensions:\n" + "\t\t(9.45x1.57x0.39)''/(8x4x1)cm (LxWxH) \n"  + "\t5 Package Includes:\n" + "\t\t1 x Smart Watch \n"  + "\t\t1 x USB Cable \n"  + "\t\t1 x User Manual \n" ,
+                    "\n" + "\t1 Material: Silicone and Aluminium \n" + "\t2 Weight:2.68Oz/76g \n" + "\t3 Color:White \n" + "\t4 Dimensions:\n" + "\t\t(9.45x1.57x0.39)''/(8x4x1)cm (LxWxH) \n"  + "\t5 Package Includes:\n" + "\t\t1 x Smart Watch \n"  + "\t\t1 x USB Cable \n"  + "\t\t1 x User Manual \n"
+            };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +69,35 @@ public class SearchResultActivity extends ActionBarActivity {
 
         //Show backButton
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
+        // Each row in the list stores country name, currency and flag
+        List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
+
+        for (int i = 0; i < 10; i++) {
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("txt", "Product : " + products[i]);
+            hm.put("cur", "Specifications : " + currency[i]);
+            hm.put("flag", Integer.toString(flags[i]));
+            aList.add(hm);
+        }
+
+        // Keys used in Hashmap
+        String[] from = {"flag", "txt", "cur","ratingBar"};
+
+        // Ids of views in listview_layout
+        int[] to = {R.id.flag, R.id.txt, R.id.cur};
+
+        // Instantiating an adapter to store each items
+        // R.layout.listview_layout defines the layout of each item
+        SimpleAdapter adapter = new SimpleAdapter(getBaseContext(), aList, R.layout.listview_layout, from, to);
+
+        // Getting a reference to listview of main.xml layout file
+        ListView listView = (ListView) findViewById(R.id.listview);
+
+        // Setting the adapter to the listView
+        listView.setAdapter(adapter);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -26,6 +105,7 @@ public class SearchResultActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_search_result, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -42,8 +122,46 @@ public class SearchResultActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+
+
+
     public void btnGoToProducttClick(View view){
         Intent intent = new Intent(this, ProductPage.class);
         startActivity(intent);
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
