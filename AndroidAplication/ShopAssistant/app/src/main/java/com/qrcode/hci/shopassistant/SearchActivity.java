48178b986f817ac5b2c75b0324eecf12809c5465
marti-org <martinorava@seznam.cz>
2015-05-03 15:36:42 +0300

    Coverflow on search page

--- AndroidAplication/ShopAssistant/app/src/main/java/com/qrcode/hci/shopassistant/SearchActivity.java
+++ AndroidAplication/ShopAssistant/app/src/main/java/com/qrcode/hci/shopassistant/SearchActivity.java
@@ -3,14 +3,33 @@ package com.qrcode.hci.shopassistant;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewSwitcher;
+
+import java.util.ArrayList;
+
+import it.moondroid.coverflow.components.ui.containers.FeatureCoverFlow;
 
 
 public class SearchActivity extends ActionBarActivity {
+
+
+    //CoverFlow Variables
+    private FeatureCoverFlow mCoverFlow;
+    private CoverFlowAdapter mAdapter;
+    private ArrayList<GameEntity> mData = new ArrayList<>(0);
+    private TextSwitcher mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +49,69 @@ public class SearchActivity extends ActionBarActivity {
         ArrayAdapter<String> adapter =
                 new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries);
         textView.setAdapter(adapter);
+
+
+
+
+        //Cover Flow
+        mData.add(new GameEntity(R.drawable.za, R.string.za));
+        mData.add(new GameEntity(R.drawable.zb, R.string.zb));
+        mData.add(new GameEntity(R.drawable.zc, R.string.zc));
+        mData.add(new GameEntity(R.drawable.zd, R.string.zd));
+        mData.add(new GameEntity(R.drawable.zf, R.string.zf));
+        mData.add(new GameEntity(R.drawable.zg, R.string.zg));
+        mData.add(new GameEntity(R.drawable.zh, R.string.zh));
+        mData.add(new GameEntity(R.drawable.zi, R.string.zi));
+        mData.add(new GameEntity(R.drawable.zj, R.string.zj));
+        mData.add(new GameEntity(R.drawable.zk, R.string.zk));
+
+
+        mTitle = (TextSwitcher) findViewById(R.id.title);
+        mTitle.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                LayoutInflater inflater = LayoutInflater.from(SearchActivity.this);
+                TextView textView = (TextView) inflater.inflate(R.layout.item_title, null);
+                return textView;
+            }
+        });
+        Animation in = AnimationUtils.loadAnimation(this, R.anim.slide_in_top);
+        Animation out = AnimationUtils.loadAnimation(this, R.anim.slide_out_bottom);
+        mTitle.setInAnimation(in);
+        mTitle.setOutAnimation(out);
+
+        mAdapter = new CoverFlowAdapter(this);
+        mAdapter.setData(mData);
+        mCoverFlow = (FeatureCoverFlow) findViewById(R.id.coverflow);
+        mCoverFlow.setAdapter(mAdapter);
+
+        mCoverFlow.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(SearchActivity.this,
+                        getResources().getString(mData.get(position).titleResId),
+                        Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(SearchActivity.this, ProductPage.class);
+                startActivity(intent);
+
+
+
+            }
+        });
+
+        mCoverFlow.setOnScrollPositionListener(new FeatureCoverFlow.OnScrollPositionListener() {
+            @Override
+            public void onScrolledToPosition(int position) {
+                mTitle.setText(getResources().getString(mData.get(position).titleResId));
+            }
+
+            @Override
+            public void onScrolling() {
+                mTitle.setText("");
+            }
+        });
     }
 
 
